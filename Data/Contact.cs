463feb701fec18cc6e6dcbc1e6c330f4ad32d46c
Romaric <romaric.miaka.sio@gmail.com>
2023-06-30 03:22:10 +0200

    model deserializer

---
+++ Data/Contact.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorDynamic365.Data;
+
+
+public class Rootobject
+{
+    public string odatacontext { get; set; }
+    public Value[] value { get; set; }
+}
+
+public class Value
+{
+    public string odataetag { get; set; }
+    public string contactid { get; set; }
+    public string firstname { get; set; }
+    public string lastname { get; set; }
+    public string fullname { get; set; }
+    public string jobtitle { get; set; }
+    public string telephone1 { get; set; }
+    public string emailaddress1 { get; set; }
+}
+
